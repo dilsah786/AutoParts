2195060ec15c44fee57e081a6e2e16a31b5fb56e
Raksha97-maker <rakshakusumakar789@gmail.com>
2023-05-19 10:49:48 +0530

    login

--- JS/login.js
+++ JS/login.js
@@ -14,3 +14,34 @@ signup.addEventListener("click", GoToSignup)
 function GoToSignup() {
     window.location.href = "../Signup/signup.html"
 }
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById("input2").value;
+    const password = document.getElementById("password").value;
+
+
+    const users = JSON.parse(localStorage.getItem("userinfo"));
+    let loggedIn = false;
+    if (email === "" || password === "") {
+        alert("Please fill in all the details.");
+    } else if (Array.isArray(users) && users.length > 0) {
+        for (let i = 0; i < users.length; i++) {
+            const user = users[i];
+            if (user.email === email && user.password === password) {
+                loggedIn = true;
+                break;
+            }
+        }
+        if (loggedIn) {
+            alert("Login Successful"); // window.location.href=""
+        } else {
+            alert("Invalid email address or password");
+        }
+    }
+
+
+});
